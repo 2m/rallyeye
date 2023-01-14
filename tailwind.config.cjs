b4200c4d6a455972bc0e4556cd3850307612e370
Martynas Mickevičius <self@2m.lt>
2023-01-14 20:39:36 +0200

    Do not store deployed URL in environment

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./target/scala-3.2.1/rallyeye-scala-fastopt/*.js", "./target/scala-3.2.1/rallyeye-scala-opt/*.js"],
+  content: [
+    "./index.html",
+    "./target/scala-3.2.1/rallyeye-scala-fastopt/*.js",
+    "./target/scala-3.2.1/rallyeye-scala-opt/*.js"
+  ],
   theme: {
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
