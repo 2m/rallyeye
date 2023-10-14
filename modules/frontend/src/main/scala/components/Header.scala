86679b754dbe0bb9cbb765dbc23885203b9bd3a5
Martynas Mickevičius <self@2m.lt>
2023-10-14 23:33:35 +0300

    Use inner shadow (#97)

--- modules/frontend/src/main/scala/components/Header.scala
+++ modules/frontend/src/main/scala/components/Header.scala
@@ -47,7 +47,7 @@ case class Header(
 
   def render() =
     navTag(
-      cls := "bg-white border-gray-200 px-4 lg:px-6 py-2.5 shadow-lg",
+      cls := "bg-white border-gray-200 px-4 lg:px-6 py-2.5",
       div(
         cls := "flex flex-wrap justify-between items-center mx-auto max-w-screen-xl",
         a(
